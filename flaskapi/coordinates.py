9d3c76bd3fa3ea47d6e296f38265d3a397783f2f
TovarasulTony <tonyfrum@yahoo.com>
2022-02-23 19:08:33 +0200

    curl test

--- flaskapi/coordinates.py
+++ flaskapi/coordinates.py
@@ -4,4 +4,6 @@ def get_lat_lon(city):
   address = city
   geolocator = Nominatim(user_agent="Your_Name")
   location = geolocator.geocode(address)
+  print(55555555)
+  print(location.address)
   return location.latitude, location.longitude 
