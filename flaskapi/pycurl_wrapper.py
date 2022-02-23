55ea0513a7020fc7ca17de6328b5fa9a63b73f61
TovarasulTony <tonyfrum@yahoo.com>
2022-02-23 21:11:51 +0200

    curl test

---
+++ flaskapi/pycurl_wrapper.py
@@ -0,0 +1,20 @@
+
+
+def make_api_call(lat, lon, call_type):
+  buffer = BytesIO()
+  c = pycurl.Curl()
+
+  if call_type == API_CALL_TYPE.No_Date:
+    c.setopt(c.URL, 'api.openweathermap.org/data/2.5/weather?lat=' + str(lat) + '&lon=' + str(lon) + '&appid=' + 'a380c85701c2082a4ac12ec49f670631')
+  elif call_type == API_CALL_TYPE.Date:
+    c.setopt(c.URL, 'https://api.openweathermap.org/data/2.5/onecall?lat=' + str(lat) + '&lon=' + str(lon) + '&exclude=current,minutely,hourly,alerts&appid=' + 'a380c85701c2082a4ac12ec49f670631')
+  else:
+    return None
+
+  c.setopt(c.WRITEDATA, buffer)
+  c.setopt(c.CAINFO, certifi.where())
+  c.perform()
+  c.close()
+  body = buffer.getvalue()
+  my_json = buffer.getvalue().decode('utf8')
+  return my_json
--- flaskapi/routes.py
+++ flaskapi/routes.py
@@ -1,6 +1,7 @@
 from flaskapi import app
 from flask import jsonify, request
 from flaskapi.coordinates import get_lat_lon
+from flaskapi.pycurl_wrapper import make_api_call
 from datetime import timezone
 from dateutil import parser as time_parser
 from flaskapi.enums import API_CALL_TYPE
@@ -11,24 +12,6 @@ import json
 from io import BytesIO
 
 
-
-def make_api_call(lat, lon, call_type):
-  buffer = BytesIO()
-  c = pycurl.Curl()
-  if call_type == API_CALL_TYPE.No_Date:
-    c.setopt(c.URL, 'api.openweathermap.org/data/2.5/weather?lat=' + str(lat) + '&lon=' + str(lon) + '&appid=' + 'a380c85701c2082a4ac12ec49f670631')
-  elif call_type == API_CALL_TYPE.Date:
-    c.setopt(c.URL, 'https://api.openweathermap.org/data/2.5/onecall?lat=' + str(lat) + '&lon=' + str(lon) + '&exclude=current,minutely,hourly,alerts&appid=' + 'a380c85701c2082a4ac12ec49f670631')
-  else:
-    return None
-  c.setopt(c.WRITEDATA, buffer)
-  c.setopt(c.CAINFO, certifi.where())
-  c.perform()
-  c.close()
-
-  body = buffer.getvalue()
-  my_json = buffer.getvalue().decode('utf8')
-  return my_json
 
 @app.route("/ping")
 @app.route("/ping/")
@@ -45,11 +28,13 @@ def ping():
 def forecast(city):
   at = request.args.get('at', type=str)
   lat, lon = get_lat_lon(city)
+
   if lat == None:
     return jsonify({
       "error": "Cannot find country/region/city '" + city + "'",
       "error_code": "country_not_found"
     }), 404
+
   if at != None:
     return handle_at_arg_case(at, lat, lon)
   else:
@@ -67,20 +52,24 @@ def handle_at_arg_case(at, lat, lon):
   timestamp_arg = yourdate.replace(tzinfo=timezone.utc).timestamp()
   return_json = make_api_call(lat, lon, API_CALL_TYPE.Date)
   return_json = json.loads(return_json)
-  one_day_offset = 86400
-  if return_json["daily"][0]["dt"] - one_day_offset > timestamp_arg:
+  ONE_DAY_UNIX_OFFSET = 86400
+
+  if return_json["daily"][0]["dt"] - ONE_DAY_UNIX_OFFSET > timestamp_arg:
     return jsonify({
       "error": "Date is in the past",
       "error_code": "invalid date"
     }), 404
+
   for ele in return_json["daily"]:
-    if timestamp_arg >= ele["dt"] - one_day_offset and timestamp_arg <= ele["dt"]:
+    if timestamp_arg >= ele["dt"] - ONE_DAY_UNIX_OFFSET and timestamp_arg <= ele["dt"]:
+      CELSIUS_KELVIN_OFFSET = 273.15 
       return jsonify({
         str(ele["weather"][0]["main"]): str(ele["weather"][0]["description"]),
         "humidity": str(round(ele["humidity"], 2)) + "%",
         "pressure": str(round(ele["pressure"], 2)) + " hPa",
-        "temperature": str(round(ele["temp"]["day"] - 273.15, 1)) + "C"
+        "temperature": str(round(ele["temp"]["day"] - CELSIUS_KELVIN_OFFSET, 1)) + "C"
       })
+
   return jsonify({
       "error": "Date is further in the future than supported",
       "error_code": "invalid date"
