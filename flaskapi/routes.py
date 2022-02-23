0c56589f67239cbf65c34ef10ba58d1a72433fc6
TovarasulTony <tonyfrum@yahoo.com>
2022-02-23 20:48:18 +0200

    curl test

--- flaskapi/routes.py
+++ flaskapi/routes.py
@@ -19,7 +19,7 @@ def make_api_call(lat, lon, call_type):
     c.setopt(c.URL, 'api.openweathermap.org/data/2.5/weather?lat=' + str(lat) + '&lon=' + str(lon) + '&appid=' + 'a380c85701c2082a4ac12ec49f670631')
   elif call_type == API_CALL_TYPE.Date:
     c.setopt(c.URL, 'https://api.openweathermap.org/data/2.5/onecall?lat=' + str(lat) + '&lon=' + str(lon) + '&exclude=current,minutely,hourly,alerts&appid=' + 'a380c85701c2082a4ac12ec49f670631')
-  else
+  else:
     return None
   c.setopt(c.WRITEDATA, buffer)
   c.setopt(c.CAINFO, certifi.where())
