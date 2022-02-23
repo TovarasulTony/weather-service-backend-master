6fada6b620e6240ad158a341176f25d715dc9a26
TovarasulTony <tonyfrum@yahoo.com>
2022-02-23 15:59:04 +0200

    syntax issues

--- flaskapi/coordinates.py
+++ flaskapi/coordinates.py
@@ -1,6 +1,6 @@
 from geopy.geocoders import Nominatim
 
-def get_lat_lon(city)
+def get_lat_lon(city):
   address = city
   geolocator = Nominatim(user_agent="Your_Name")
   location = geolocator.geocode(address)
