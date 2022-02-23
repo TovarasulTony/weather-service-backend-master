d0a2c50d15c086d1558b6efad94508523f33e3ea
TovarasulTony <tonyfrum@yahoo.com>
2022-02-23 21:03:31 +0200

    curl test

--- flaskapi/routes.py
+++ flaskapi/routes.py
@@ -28,13 +28,7 @@ def make_api_call(lat, lon, call_type):
 
   body = buffer.getvalue()
   my_json = buffer.getvalue().decode('utf8')
-  print(my_json)
   return my_json
-
-def find_clouds(weather):
-  for element in weather:
-    if element["main"] == "Clouds":
-      return element["description"]
 
 @app.route("/ping")
 @app.route("/ping/")
@@ -50,52 +44,47 @@ def ping():
 @app.route("/forecast/<string:city>/")
 def forecast(city):
   at = request.args.get('at', type=str)
-  if at != None:
-    yourdate = time_parser.isoparse(at.replace(" ", "+")) # dirty hack, I don't know how to fix this or if this breaks something else :(
-    timestamp_arg = yourdate.replace(tzinfo=timezone.utc).timestamp()
-    print(timestamp_arg)
-    lat, lon = get_lat_lon(city)
-    if lat == None:
-      return jsonify({
-        "error": "Cannot find country '" + city + "'",
-        "error_code": "country_not_found"
-      }), 404
-    return_json = make_api_call(lat, lon, API_CALL_TYPE.Date)
-    return_json = json.loads(return_json)
-    if return_json["daily"][0]["dt"] > timestamp_arg:
-      return jsonify({
-        "error": "Date is in the past",
-        "error_code": "invalid date"
-      }), 404
-    one_day_offset = 86400
-    for ele in return_json["daily"]:
-      if timestamp_arg >= ele["dt"] and timestamp_arg < ele["dt"] + one_day_offset:
-        return jsonify({
-          str(ele["weather"][0]["main"]): str(ele["weather"][0]["description"]),
-          "humidity": str(round(ele["humidity"], 2)) + "%",
-          "pressure": str(round(ele["pressure"], 2)) + " hPa",
-          "temperature": str(round(ele["temp"]["day"] - 273.15, 1)) + "C"
-        })
-    return jsonify({
-        "error": "Date is further in the future than supported",
-        "error_code": "invalid date"
-      }), 404
-
   lat, lon = get_lat_lon(city)
   if lat == None:
     return jsonify({
-      "error": "Cannot find country '" + city + "'",
+      "error": "Cannot find country/region/city '" + city + "'",
       "error_code": "country_not_found"
     }), 404
+  if at != None:
+    handle_at_arg_case(at)
+  else:
+    return_json = make_api_call(lat, lon, API_CALL_TYPE.No_Date)
+    return_json = json.loads(return_json)
+    return jsonify({
+      str(return_json["weather"][0]["main"]): str(return_json["weather"][0]["description"]),
+      "humidity": str(round(return_json["main"]["humidity"], 2)) + "%",
+      "pressure": str(round(return_json["main"]["pressure"], 2)) + " hPa",
+      "temperature": str(round(return_json["main"]["temp"] - 273.15, 1)) + "C"
+    })
 
-  return_json = make_api_call(lat, lon, API_CALL_TYPE.No_Date)
+def handle_at_arg_case(at):
+  yourdate = time_parser.isoparse(at.replace(" ", "+")) # dirty hack, I don't know how to fix this or if this breaks something else :(
+  timestamp_arg = yourdate.replace(tzinfo=timezone.utc).timestamp()
+  return_json = make_api_call(lat, lon, API_CALL_TYPE.Date)
   return_json = json.loads(return_json)
+  if return_json["daily"][0]["dt"] > timestamp_arg:
+    return jsonify({
+      "error": "Date is in the past",
+      "error_code": "invalid date"
+    }), 404
+  one_day_offset = 86400
+  for ele in return_json["daily"]:
+    if timestamp_arg >= ele["dt"] - 500 and timestamp_arg < ele["dt"] + one_day_offset:
+      return jsonify({
+        str(ele["weather"][0]["main"]): str(ele["weather"][0]["description"]),
+        "humidity": str(round(ele["humidity"], 2)) + "%",
+        "pressure": str(round(ele["pressure"], 2)) + " hPa",
+        "temperature": str(round(ele["temp"]["day"] - 273.15, 1)) + "C"
+      })
   return jsonify({
-    str(return_json["weather"][0]["main"]): str(return_json["weather"][0]["description"]),
-    "humidity": str(round(return_json["main"]["humidity"], 2)) + "%",
-    "pressure": str(round(return_json["main"]["pressure"], 2)) + " hPa",
-    "temperature": str(round(return_json["main"]["temp"] - 273.15, 1)) + "C"
-  })
+      "error": "Date is further in the future than supported",
+      "error_code": "invalid date"
+    }), 404
 
 @app.errorhandler(500)
 def internal_error(error):
